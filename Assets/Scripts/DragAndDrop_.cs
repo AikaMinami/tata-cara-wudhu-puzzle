416c356b3329862b13794a32621c506e3853f1a4
AikaMinami <aikatan.aikachan@gmail.com>
2023-01-15 13:20:53 +0700

    inital file

---
+++ Assets/Scripts/DragAndDrop_.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.SceneManagement;
+public class DragAndDrop_ : MonoBehaviour
+{
+    public Sprite[] Levels;
+
+    public GameObject EndMenu;
+    public GameObject SelectedPiece;
+    public int pieceAmount =4;
+    public GameObject hintImage;
+    int OIL = 1;    
+    int currentLevel;
+    public int PlacedPieces = 0;
+    void Start()
+    {
+        Debug.Log("Current Level:" + PlayerPrefs.GetInt("Level"));
+        currentLevel = PlayerPrefs.GetInt("Level");
+        EndMenu.SetActive(false);
+        for (int i = 0;i < pieceAmount; i++)
+        {
+            // GameObject.FindGameObjectWithTag("ImageMask").GetComponent<SpriteRenderer>().sprite = Levels[currentLevel];
+            GameObject.Find("Puzzle Piece").transform.Find("Piece (" + i + ")").transform.Find("Puzzle").GetComponent<SpriteRenderer>().sprite = Levels[currentLevel];
+            hintImage.GetComponent<SpriteRenderer>().sprite = Levels[currentLevel];
+        }
+        
+    }
+
+    void Update()
+    {
+        // bool fal = false;
+        // if (Input.GetMouseButtonDown(0) && fal)
+        // {
+        //     RaycastHit2D hit = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(Input.mousePosition), Vector2.zero);
+        //     if (hit.transform.CompareTag("Puzzle"))
+        //     {
+        //         if (!hit.transform.GetComponent<piceseScript>().InRightPosition)
+        //         {
+        //             SelectedPiece = hit.transform.gameObject;
+        //             SelectedPiece.GetComponent<piceseScript>().Selected = true;
+        //             SelectedPiece.GetComponent<SortingGroup>().sortingOrder = OIL;
+        //             OIL++;
+        //         }
+        //     } 
+        //     else 
+        //     {
+        //         Debug.Log("dont do anything");
+        //     }
+        // }
+
+        // if (Input.GetMouseButtonUp(0) && fal)
+        // {
+        //     if (SelectedPiece != null)
+        //     {
+        //         SelectedPiece.GetComponent<piceseScript>().Selected = false;
+        //         SelectedPiece = null;
+        //     }
+        // }
+        // if (SelectedPiece != null && fal)
+        // {
+        //     Vector3 MousePoint = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        //     SelectedPiece.transform.position = new Vector3(MousePoint.x,MousePoint.y,0);
+        // }             
+        // if (PlacedPieces == pieceAmount)
+        // {
+        //     EndMenu.SetActive(true);
+        // }
+    }
+    public void NextLevel()
+    {
+        PlayerPrefs.SetInt("Level", PlayerPrefs.GetInt("Level")+1);
+        SceneManager.LoadScene("Game");
+    }
+
+    public void BacktoMenu()
+    {
+        SceneManager.LoadScene("Menu");
+    }
+}
---
+++ Assets/Scripts/menuScript_.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class menuScript_ : MonoBehaviour
+{
+    int currentLevel;
+    int totalLevel = 8;
+
+    private void Start() {
+        currentLevel = PlayerPrefs.GetInt("Level");
+    }
+    public void PlayNextGame()
+    {
+        int newLevel = currentLevel+1;
+        if(currentLevel == 7)
+        {
+            newLevel = 0;
+        } 
+        Debug.Log(newLevel);
+        PlayerPrefs.SetInt("Level", newLevel);
+        PlayGame();
+    }
+    public void PlayPrevGame()
+    {
+        int newLevel = currentLevel-1;
+        if(currentLevel == 0)
+        {
+            newLevel = 0;
+        } 
+        Debug.Log(newLevel);
+        PlayerPrefs.SetInt("Level", newLevel);
+        PlayGame();
+    }
+
+    void PlayGame()
+    {
+        SceneManager.LoadScene("PuzzleWudhu");
+    }
+    public void PlayNewGame()
+    {
+        PlayerPrefs.SetInt("Level", 0);
+        PlayGame();
+    }
+
+
+}
